06168d793f4c0f6afc15cb080f4a74f2afdd0132
Firstname Lastname <asupunjayasooriya@gmail.com>
2022-08-19 23:39:34 +0530

    start

---
+++ lib/main.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  return runApp(
+    MaterialApp(
+      home: Scaffold(
+        backgroundColor: Colors.redAccent,
+        appBar: AppBar(
+          title: Text('Xylophone'),
+          backgroundColor: Colors.red,
+        ),
+      ),
+    ),
+  );
+}
